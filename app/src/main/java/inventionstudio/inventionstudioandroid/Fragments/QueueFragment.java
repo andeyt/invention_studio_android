0deaeaf817cb3bb575c5a38f0ff4508a6a87979b
Aman <aaggarwal73@gatech.edu>
2017-11-26 14:20:41 -0500

    middle of adding stuff for queue

--- app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/QueueFragment.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/QueueFragment.java
@@ -2,10 +2,12 @@ package inventionstudio.inventionstudioandroid.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import inventionstudio.inventionstudioandroid.R;
 
@@ -28,4 +30,11 @@ public class QueueFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_queue, container, false);
     }
 
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Inflate the layout for this fragment
+        TextView text = view.findViewById(R.id.textView);
+        text.setText("To join a queue, head to the kiosk closest to the machine" +
+                " you're interested in. Find a PI (with a green armband) for more info.");
+    }
 }
