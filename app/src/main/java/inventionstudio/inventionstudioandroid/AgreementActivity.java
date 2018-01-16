e3ab6dcb2befa8af64e93564bf0c640b9da9f92c
Maxwell <maximusbroomhandle@gmail.com>
2018-01-16 17:16:38 -0500

    Agreement Activity Complete

--- app/src/main/java/inventionstudio/inventionstudioandroid/AgreementActivity.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/AgreementActivity.java
@@ -1,8 +1,21 @@
 package inventionstudio.inventionstudioandroid;
 
-/**
- * Created by maxim_000 on 1/16/2018.
- */
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class AgreementActivity {
+public class AgreementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agreement);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
 }
