b41edbdfb4a469684f0a928215e470f300f46881
Maxwell <maximusbroomhandle@gmail.com>
2018-01-22 16:20:56 -0500

    Dummy lists for Spinners, style changes in Feedback and Report a Problem

--- app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/FeedbackFragment.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/FeedbackFragment.java
@@ -6,11 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 
 import inventionstudio.inventionstudioandroid.R;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,9 +27,20 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        getActivity().setTitle("Feedback");
-
         View rootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+
+        // EditText Instantiation
+        EditText textInput = (EditText) rootView.findViewById(R.id.plain_text_input);
+        textInput.setBackgroundResource(R.drawable.edittext_border);
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner1);
+        spinner.setBackgroundResource(R.drawable.spinner_border);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.feedback_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
 
         return rootView;
     }
