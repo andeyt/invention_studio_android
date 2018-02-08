4ef1e77f9627050a8bf6628e2fb2759ce3a89743
Maxwell <maximusbroomhandle@gmail.com>
2018-02-08 16:50:03 -0500

    Queue Fragment API interface - first attempt, null members list

--- app/src/main/java/inventionstudio/inventionstudioandroid/API/SumsApiService.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/API/SumsApiService.java
@@ -1,7 +1,9 @@
 package inventionstudio.inventionstudioandroid.API;
 
 import java.util.List;
+
 import inventionstudio.inventionstudioandroid.Model.Machine;
+import inventionstudio.inventionstudioandroid.Model.QueueMember;
 import retrofit2.*;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -12,7 +14,9 @@ import retrofit2.http.Query;
 
 public interface SumsApiService {
 
-    @POST("equipmentGroup_tools")
-    Call<List<Machine>> getMachineList(@Query("DepartmentID") int id);
+    @POST("equipmentGroup_tools?DepartmentID=8")
+    Call<List<Machine>> getMachineList();
 
+    @POST("equipmentGroup_queues")
+    Call<List<QueueMember>> getQueueLists(@Query("DepartmentID") int id, @Query("userName") String userName);
 }
---
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Model/QueueMember.java
@@ -0,0 +1,96 @@
+package inventionstudio.inventionstudioandroid.Model;
+
+/**
+ * Created by Maxwell Broom on 2/8/2018.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class QueueMember {
+
+    @SerializedName("queueGroupId")
+    @Expose
+    private Integer queueGroupId;
+
+    @SerializedName("memberName")
+    @Expose
+    private String memberName;
+
+    @SerializedName("memberUserName")
+    @Expose
+    private String memberUserName;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("hasBeenNotified")
+    @Expose
+    private Boolean hasBeenNotified;
+
+    @SerializedName("memberMinutesRemaining")
+    @Expose
+    private Integer memberMinutesRemaining;
+
+    @SerializedName("memberQueueLocation")
+    @Expose
+    private Integer memberQueueLocation;
+
+    public Integer getQueueGroupId() {
+        return queueGroupId;
+    }
+
+    public void setQueueGroupId(Integer queueGroupId) {
+        this.queueGroupId = queueGroupId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberUserName() {
+        return memberUserName;
+    }
+
+    public void setMemberUserName(String memberUserName) {
+        this.memberUserName = memberUserName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getHasBeenNotified() {
+        return hasBeenNotified;
+    }
+
+    public void setHasBeenNotified(Boolean hasBeenNotified) {
+        this.hasBeenNotified = hasBeenNotified;
+    }
+
+    public Integer getMemberMinutesRemaining() {
+        return memberMinutesRemaining;
+    }
+
+    public void setMemberMinutesRemaining(Integer memberMinutesRemaining) {
+        this.memberMinutesRemaining = memberMinutesRemaining;
+    }
+
+    public Integer getMemberQueueLocation() {
+        return memberQueueLocation;
+    }
+
+    public void setMemberQueueLocation(Integer memberQueueLocation) {
+        this.memberQueueLocation = memberQueueLocation;
+    }
+
+}
