8d39f6a728d1329c8e12a5bfbdac03e131cd6668
Maxwell <maximusbroomhandle@gmail.com>
2018-02-26 21:44:49 -0500

    themeFragment - changed ListView to RecyclerView.

--- app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/themeFragment.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Fragments/themeFragment.java
@@ -2,16 +2,24 @@ package inventionstudio.inventionstudioandroid.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import inventionstudio.inventionstudioandroid.Adapters.MyRecyclerViewAdapter;
+import inventionstudio.inventionstudioandroid.Model.SimpleDividerItemDecoration;
 import inventionstudio.inventionstudioandroid.R;
 
+import static android.content.ContentValues.TAG;
+
 public class themeFragment extends Fragment {
+
+    MyRecyclerViewAdapter adapter;
 
     public themeFragment() {
         // Required empty public constructor
@@ -21,21 +29,22 @@ public class themeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_theme, container, false);
-        ListView listview = rootView.findViewById(R.id.theme_list);
-        String[] themes = new String[]{"Light", "Dark"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, themes);
-        listview.setAdapter(adapter);
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                int position = i;
-                switch(position) {
-                    case 0:
-                        
-                }
-            }
-        });
+        ArrayList<String> themes = new ArrayList<>();
+        // Will need to add more available themes here
+        themes.add("Light");
+        themes.add("Dark");
+        // Create view, set layout manager, adapter, and dividers
+        RecyclerView list = rootView.findViewById(R.id.theme_list);
+        list.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        adapter = new MyRecyclerViewAdapter(this.getContext(), themes);
+        list.setAdapter(adapter);
+        list.addItemDecoration(new SimpleDividerItemDecoration(this.getContext()));
 
         return rootView;
+    }
+
+    // Deals with a click event
+    public void onItemClick(View view, int position) {
+        Log.d(TAG, "onItemClick: " + position);
     }
 }
