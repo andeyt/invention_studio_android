d16d8f60b4aba754eda0ad14904babdeb18bd076
Rishab Kaup <rkaup3@gatech.edu>
2018-04-06 12:09:42 -0400

    Data messages for push notifications and changed app status

---
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Model/AppStatus.java
@@ -0,0 +1,54 @@
+package inventionstudio.inventionstudioandroid.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Rishab K on 4/6/2018.
+ */
+
+public class AppStatus {
+
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public AppStatus() {
+    }
+
+    /**
+     *
+     * @param message
+     * @param title
+     */
+    public AppStatus(String title, String message) {
+        super();
+        this.title = title;
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
