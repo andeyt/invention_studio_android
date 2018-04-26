2f33678f98ddf89aac049ca45cd86286464a5afa
Rishab Kaup <rkaup3@gmail.com>
2018-04-26 13:28:48 -0400

    Remove disclaimer, change agreement page and agreement action

--- app/src/main/java/inventionstudio/inventionstudioandroid/Activities/AgreementActivity.java
+++ app/src/main/java/inventionstudio/inventionstudioandroid/Activities/AgreementActivity.java
@@ -37,73 +37,11 @@ public class AgreementActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                connectAndGetApiData();
+                Intent intent = new Intent(getApplicationContext(), LoadingActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
 
-    /**
-     * method created to connect to the SUMS API and pull down the list of studio
-     * members to confirm if user is a part of the studio
-     */
-    public void connectAndGetApiData() {
-
-        // Create a retrofit to contact the API and pass in necessary arguments
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-
-        // Create API class and grab user info from stored app data
-        SumsApiService sumsApiService = retrofit.create(SumsApiService.class);
-        SharedPreferences prefs = this.getSharedPreferences(USER_PREFERENCES, MODE_PRIVATE);
-        String username = prefs.getString("username", "");
-
-        // Call the API via arguments and set method enqueue to parse through
-        // all members and find if the logged in user is a part of the studio
-        String otp = prefs.getString("otp", "");
-        call = sumsApiService.getUserGroups(username, otp);
-        call.enqueue(new Callback<List<UserGroups>>() {
-            @Override
-            public void onResponse(Call<List<UserGroups>> call, Response<List<UserGroups>> response) {
-
-                // Parse through list of usergroups for the user, if one is
-                // the studio, log them in
-                List<UserGroups> groups = response.body();
-                studioMember = false;
-                if (groups != null) {
-                    for (UserGroups u : groups) {
-                        if (u.getEquipmentGroupId() == 8) {
-                            studioMember = true;
-                            SharedPreferences prefs = getSharedPreferences(USER_PREFERENCES, MODE_PRIVATE);
-                            SharedPreferences.Editor editor = prefs.edit();
-
-                            editor.putString("name", u.getUserName());
-                            editor.apply();
-                        }
-                    }
-                }
-
-                // If a part of the studio, send to main activity
-                // else, show agreement page so they can sign it and log in
-                if (studioMember) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(AgreementActivity.this, "Please sign the User Agreement", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-            /**
-             * Give a toast if any issue occurs with connecting to the API
-             */
-            @Override
-            public void onFailure(Call<List<UserGroups>> call, Throwable throwable) {
-                Toast.makeText(AgreementActivity.this, "An Error Occurred", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
